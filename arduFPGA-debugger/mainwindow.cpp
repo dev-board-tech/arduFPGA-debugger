0a43c5855d66ca6c5bc49a28549d1903868a1bab
Iulian Gheorghiu <morgoth@devboard.tech>
2020-10-03 22:39:29 +0300

    Add option to watch up to 8 memory windows and up to 16 IO windows.

--- arduFPGA-debugger/mainwindow.cpp
+++ arduFPGA-debugger/mainwindow.cpp
@@ -12,15 +12,59 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     this->setWindowTitle(mainTitle);
     serialPortWindow = new serialPortSetup(this);
-    memoryWin = new memoryWindow(this, &serial);
-    ioWin = new ioWindow(this, &serial);
+    memoryWin[0] = new memoryWindow(this, &serial);
+    memoryWin[1] = new memoryWindow(this, &serial);
+    memoryWin[2] = new memoryWindow(this, &serial);
+    memoryWin[3] = new memoryWindow(this, &serial);
+    memoryWin[4] = new memoryWindow(this, &serial);
+    memoryWin[5] = new memoryWindow(this, &serial);
+    ioWin[0] = new ioWindow(this, &serial);
+    ioWin[1] = new ioWindow(this, &serial);
+    ioWin[2] = new ioWindow(this, &serial);
+    ioWin[3] = new ioWindow(this, &serial);
+    ioWin[4] = new ioWindow(this, &serial);
+    ioWin[5] = new ioWindow(this, &serial);
+    ioWin[6] = new ioWindow(this, &serial);
+    ioWin[7] = new ioWindow(this, &serial);
+    ioWin[8] = new ioWindow(this, &serial);
+    ioWin[9] = new ioWindow(this, &serial);
+    ioWin[10] = new ioWindow(this, &serial);
+    ioWin[11] = new ioWindow(this, &serial);
+    ioWin[12] = new ioWindow(this, &serial);
+    ioWin[13] = new ioWindow(this, &serial);
+    ioWin[14] = new ioWindow(this, &serial);
+    ioWin[15] = new ioWindow(this, &serial);
+
+    fileDialog = new QFileDialog(parent);
+    fileDialog->setReadOnly(true);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete serialPortWindow;
-    delete memoryWin;
-    delete ioWin;
+    delete memoryWin[5];
+    delete memoryWin[4];
+    delete memoryWin[3];
+    delete memoryWin[2];
+    delete memoryWin[1];
+    delete memoryWin[0];
+    delete ioWin[15];
+    delete ioWin[14];
+    delete ioWin[13];
+    delete ioWin[12];
+    delete ioWin[11];
+    delete ioWin[10];
+    delete ioWin[9];
+    delete ioWin[8];
+    delete ioWin[7];
+    delete ioWin[6];
+    delete ioWin[5];
+    delete ioWin[4];
+    delete ioWin[3];
+    delete ioWin[2];
+    delete ioWin[1];
+    delete ioWin[0];
     delete ui;
 }
 
@@ -82,12 +126,118 @@ void MainWindow::on_actionExit_triggered()
 
 
 
-void MainWindow::on_actionMemory_triggered()
+void MainWindow::on_actionOpen_File_triggered()
 {
-    memoryWin->show();
+    fileDialog->getOpenFileName(this, "Open ROM file", QString() ,QString("*.hex *.bin"));
 }
 
-void MainWindow::on_actionIO_triggered()
+void MainWindow::on_actionOpen_Memory_Window_1_triggered()
 {
-    ioWin->show();
+    memoryWin[0]->show();
+}
+
+void MainWindow::on_actionOpen_Memory_Window_2_triggered()
+{
+    memoryWin[1]->show();
+}
+
+void MainWindow::on_actionOpen_Memory_Window_3_triggered()
+{
+    memoryWin[2]->show();
+}
+
+void MainWindow::on_actionOpen_Memory_Window_4_triggered()
+{
+    memoryWin[3]->show();
+}
+
+void MainWindow::on_actionOpen_Memory_Window_5_triggered()
+{
+    memoryWin[4]->show();
+}
+
+void MainWindow::on_actionOpen_Memory_Window_6_triggered()
+{
+    memoryWin[5]->show();
+}
+
+
+void MainWindow::on_actionOpen_IO_Window_1_triggered()
+{
+    ioWin[0]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_2_triggered()
+{
+    ioWin[1]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_3_triggered()
+{
+    ioWin[2]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_4_triggered()
+{
+    ioWin[3]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_5_triggered()
+{
+    ioWin[4]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_6_triggered()
+{
+    ioWin[5]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_7_triggered()
+{
+    ioWin[6]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_8_triggered()
+{
+    ioWin[7]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_9_triggered()
+{
+    ioWin[8]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_10_triggered()
+{
+    ioWin[9]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_11_triggered()
+{
+    ioWin[10]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_12_triggered()
+{
+    ioWin[11]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_13_triggered()
+{
+    ioWin[12]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_14_triggered()
+{
+    ioWin[13]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_15_triggered()
+{
+    ioWin[14]->show();
+}
+
+void MainWindow::on_actionOpen_IO_Window_16_triggered()
+{
+    ioWin[15]->show();
 }
--- arduFPGA-debugger/mainwindow.h
+++ arduFPGA-debugger/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
 
 #include "serialportsetup.h"
 #include "memorywindow.h"
@@ -27,19 +28,43 @@ private slots:
     void on_actionOpen_COM_triggered();
     void on_actionExit_triggered();
 
-    void on_actionMemory_triggered();
-    void on_actionIO_triggered();
+    void on_actionOpen_File_triggered();
+
+    void on_actionOpen_Memory_Window_1_triggered();
+    void on_actionOpen_Memory_Window_2_triggered();
+    void on_actionOpen_Memory_Window_3_triggered();
+    void on_actionOpen_Memory_Window_4_triggered();
+    void on_actionOpen_Memory_Window_5_triggered();
+    void on_actionOpen_Memory_Window_6_triggered();
+
+    void on_actionOpen_IO_Window_1_triggered();
+    void on_actionOpen_IO_Window_2_triggered();
+    void on_actionOpen_IO_Window_3_triggered();
+    void on_actionOpen_IO_Window_4_triggered();
+    void on_actionOpen_IO_Window_5_triggered();
+    void on_actionOpen_IO_Window_6_triggered();
+    void on_actionOpen_IO_Window_7_triggered();
+    void on_actionOpen_IO_Window_8_triggered();
+    void on_actionOpen_IO_Window_9_triggered();
+    void on_actionOpen_IO_Window_10_triggered();
+    void on_actionOpen_IO_Window_11_triggered();
+    void on_actionOpen_IO_Window_12_triggered();
+    void on_actionOpen_IO_Window_13_triggered();
+    void on_actionOpen_IO_Window_14_triggered();
+    void on_actionOpen_IO_Window_15_triggered();
+    void on_actionOpen_IO_Window_16_triggered();
 
 private:
     Ui::MainWindow *ui;
     serialPortSetup *serialPortWindow;
-    memoryWindow *memoryWin;
-    ioWindow *ioWin;
+    memoryWindow *memoryWin[6];
+    ioWindow *ioWin[16];
 
     QSerialPort serial;
     QString comPort;
     QString baudRate;
 
+    QFileDialog *fileDialog;
 
 signals:
     void error(const QString &s);
